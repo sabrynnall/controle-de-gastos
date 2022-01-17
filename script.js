7716e21f474b60e94da614d7ccbea8e6e345cd18
Sabrynna <limasabrynna03@gmail.com>
2022-01-17 19:35:31 -0300

    finalizando projeto

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function capturarValorOrcamento() {
 //Atualiza os campos laterais
     atualizarInterface()
 }
-
+//Selecionam os campos nome, valor e botão
 const campoNomeDespesa = document.querySelector('.formularioEntradaDespesaNome');
 const campoValorDespesa = document.querySelector('.formularioEntradaDespesaValor');
 const buttonDespesa = document.querySelector('.formularioEntradaDespesa button');
@@ -44,9 +44,11 @@ function capturarValoresDespesa() {
     controleGastos.saldo -= valorDespesa;
 //Atualiza os campos laterais
     atualizarInterface()
+//Chamando a função para atualizar os campos de despesa
+    adicionarDespesaInterface(nomeDespesa, valorDespesa);
 }
 
-//Identifica os parágrafoos criados na lateral
+//Seleciona os parágrafoos criados na lateral
 const orcamento = document.querySelector('.secaoImpressaoResultadosOrcamento p');
 const despesas = document.querySelector('.secaoImpressaoResultadosDespesas p');
 const saldo = document.querySelector('.secaoImpressaoResultadosSaldo p');
@@ -56,4 +58,43 @@ function atualizarInterface() {
     orcamento.innerText = `+ R$ ${controleGastos.orcamento}`;
     despesas.innerText = `- R$ ${controleGastos.despesas}`;
     saldo.innerText = `R$ ${controleGastos.saldo}`;
+}
+
+const listaDespesas = document.querySelector('.containerDespesasLista')
+
+//Adicionando os campos de despesa
+function adicionarDespesaInterface(nomeDespesa, valorDespesa) {
+    const li = document.createElement('li');
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
+    const img = document.createElement('img');
+//Atualiza na interface o nome da despesa e o valor
+    h3.innerText = nomeDespesa;
+    p.innerText = `R$ ${valorDespesa}`;
+
+    img.src = 'img/trash.svg';
+    img.alt = 'Ícone Lixeira'
+//Intercepta o clique na imagem da lixeira e chama a função removerDespesa para apagar
+    img.addEventListener('click', removerDespesa)
+//Identifica o valor exato da despesa para recalcular os valores caso haja a remoção da despesa
+    li.dataset.valor = valorDespesa;
+//Informa que o h3, p e img são filhos de li
+    li.appendChild(h3);
+    li.appendChild(p);
+    li.appendChild(img);
+
+    listaDespesas.appendChild(li);
+}
+//O .target pega o elemento clicado através do evento e com ele chama-se o .parentNode que pega o pai do evento (li)
+function removerDespesa(evento) {
+    const despesaClicada = evento.target.parentNode;
+//Identifica a despesa clicada
+    const valorDespesaClicada = Number(despesaClicada.dataset.valor);
+//Remove o valor clicado do total de despesas e adiciona o valor no saldo
+    controleGastos.despesas -= valorDespesaClicada;
+    controleGastos.saldo += valorDespesaClicada;
+//Atualiza os valores nos campos laterais
+    atualizarInterface();
+    despesaClicada.remove();
+
 }
